7135870e8be63990fd6c165b239a62477f18266d
Luis Borges <borgesluisj@gmail.com>
2020-03-29 18:09:51 +0200

    index and show displayed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Product.destroy_all
 
-100.times do
+60.times do
   Product.create(
     title: Faker::Commerce.product_name,
     color: Faker::Commerce.color,
@@ -16,6 +17,6 @@
     delivery: rand(1..5),
     fabric: Faker::Commerce.material,
     size: rand(30..40),
-    photo: 'https://images.unsplash.com/photo-1552066379-e7bfd22155c5?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9'
+    photo: 'https://source.unsplash.com/random?sig=123'
     )
 end
