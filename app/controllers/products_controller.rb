9df468e4dc15a348c0df6a13780a469089f4ebb1
Luis Borges <borgesluisj@gmail.com>
2020-03-29 19:28:59 +0200

    controllers working

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,18 +9,35 @@ class ProductsController < ApplicationController
   end
 
   def new
+    @product = Product.new
   end
 
   def create
+    @product = Product.new(product_params)
+    @product.save
+    redirect_to product_path(@product)
   end
 
   def edit
+    @product = Product.find(params[:id])
   end
 
   def update
+    @product = Product.find(params[:id])
+    @product.update(product_params)
+    redirect_to product_path(@product)
   end
 
-  def destroy
+  def
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_path
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:title, :description, :price, :color, :fabric, :delivery)
   end
 
 end
