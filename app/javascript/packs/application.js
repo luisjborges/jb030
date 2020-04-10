012c53b8b4dce1ac43ae64fa200a8f492fedff2a
Luis Borges <borgesluisj@gmail.com>
2020-04-10 12:54:39 +0200

    slider styled with screen width

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,5 +7,5 @@ import "bootstrap";
 import { initUpdateNavbarOnScroll } from '../components/navbar';
 initUpdateNavbarOnScroll();
 
-import { loadDynamicBannerText } from '../components/banner';
-loadDynamicBannerText();
+// import { loadDynamicBannerText } from '../components/banner';
+// loadDynamicBannerText();
