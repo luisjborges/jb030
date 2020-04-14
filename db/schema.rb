51451401a58160b3306d3965afd7442323ac8d96
Luis Borges <borgesluisj@gmail.com>
2020-04-14 10:46:58 +0200

    added money price gem and migrated for prices

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_14_083559) do
+ActiveRecord::Schema.define(version: 2020_04_14_084438) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2020_04_14_083559) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
     t.string "sku"
+    t.integer "price_cents", default: 0, null: false
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
