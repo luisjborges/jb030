8db2f35caee3a1232d500c95861978e132da41e8
Luis Borges <borgesluisj@gmail.com>
2020-04-17 19:33:29 +0200

    updated stripe sessions

--- app/services/stripe_request_service.rb
+++ app/services/stripe_request_service.rb
@@ -1,6 +1,28 @@
 class StripeRequestService
   def call(event)
-    request = Request.find_by(checkout_session_id: event.data.object.id)
-    request.update(state: 'paid')
+    @request = Request.find_by(checkout_session_id: event.data.object.id)
+    @request.update(state: 'paid')
   end
 end
+
+
+# class BillingEventLogger
+#   def initialize(logger)
+#     @logger = logger
+#   end
+
+#   def call(event)
+#     @logger.info "BILLING:#{event.type}:#{event.id}"
+#   end
+# end
+
+
+ # def initializer(logger)
+ #    @logger = logger
+ #  end
+
+ #  def call(event)
+ #    @logger.info "BILLING:#{event.type}:#{event.id}"
+ #    @request = Request.find_by(checkout_session_id: event.data.object.id)
+ #    @request.update(state: 'paid')
+ #  end
--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -8,11 +8,17 @@ Stripe.api_key = Rails.configuration.stripe[:secret_key]
 StripeEvent.signing_secret = Rails.configuration.stripe[:signing_secret]
 
 
+# StripeEvent.configure do |events|
+#   events.subscribe 'checkout.session.completed' do |event|
+#     event.data.object
+#     # StripeCheckoutSessionService.new
+#   #   StripeRequestService.new
+#   # }
+#   end
+# end
+
 StripeEvent.configure do |events|
-  events.subscribe 'checkout.session.completed',
-    StripeCheckoutSessionService.new
-  #   StripeRequestService.new
-  # }
+  events.subscribe 'checkout.session.completed', StripeCheckoutSessionService.new
 end
 
 # StripeEvent.configure do |events|
@@ -20,6 +26,12 @@ end
 # end
 
 
+
+
+# StripeEvent.configure do |events|
+#   events.all StripeRequestService.new(Rails.logger)
+#   events.subscribe 'checkout.session.completed', StripeCheckoutSessionService.new
+# end
 
 
 # StripeEvent.configure do |events|
@@ -36,3 +48,31 @@ end
 #     Ingredient.create(name: v)
 #   end
 # end
+
+
+# StripeEvent.configure do |events|
+#   events.subscribe 'checkout.session.completed' do |event|
+#     event.class       #=> Stripe::Event
+#     event.type        #=> "charge.failed"
+#     event.data.object #=> #<Stripe::Charge:0x3fcb34c115f8>
+#   end
+
+#   events.all do |event|
+#     # Handle all event types - logging, etc.
+#   end
+# end
+
+
+# StripeEvent.configure do |events|
+#   events.subscribe 'charge.failed' do |event|
+#     # Define subscriber behavior based on the event object
+#     event.class       #=> Stripe::Event
+#     event.type        #=> "charge.failed"
+#     event.data.object #=> #<Stripe::Charge:0x3fcb34c115f8>
+#   end
+
+#   events.all do |event|
+#     # Handle all event types - logging, etc.
+#   end
+# end
+
