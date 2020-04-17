27899df9b94a3e25aec48bcb4c2960e95a6c5728
Luis Borges <borgesluisj@gmail.com>
2020-04-17 12:37:43 +0200

    deleted order vouchers migration

--- db/migrate/20200417070803_remove_price_from_order_vouchers.rb
+++
@@ -1,5 +0,0 @@
-class RemovePriceFromOrderVouchers < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :order_vouchers, :amount_currency, :integer
-  end
-end
--- db/migrate/20200417094609_drop_order_voucher.rb
+++
@@ -1,5 +0,0 @@
-class DropOrderVoucher < ActiveRecord::Migration[6.0]
-  def change
-    drop_table :order_vouchers
-  end
-end
