80cd3abed967a36b89336e17c8c33e42097aa911
Luis Borges <borgesluisj@gmail.com>
2020-04-19 08:19:44 +0200

    styling shirt and voucher shirt

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :user
-  validates :sku, :name, :description, :price, :size, :color, :fabric, :delivery, :note, :photo, presence: true
+  validates :sku, :name, :description, :price, :delivery, :note, :photo, presence: true
   has_one_attached :photo
   monetize :price_cents
   # has_many :reviews, dependent: :destroy
