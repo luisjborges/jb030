ce3b787d68365e1aefcf4cce9219d0ffa92887a9
Maik Froebisch <maik.froebisch@gmail.com>
2020-05-05 16:37:44 +0200

    add helper method find delivery

--- app/helpers/delivery_helper.rb
+++ app/helpers/delivery_helper.rb
@@ -1,6 +1,10 @@
 module DeliveryHelper
   def find_delivery(user_email, key)
     delivery = Delivery.where(email: user_email)
-    deli = delivery[0][key]
+    if delivery[0][key] != nil
+      delivery[0][key]
+    else
+      "n/a"
+    end
   end
 end
